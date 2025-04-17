86573f35a774ad1eae76c59e4c32ad3cab0ba56a
yuyu5510 <yuyu5510.chara@icloud.com>
2025-04-17 09:20:23 +0900

    homework openmp

--- 02_openmp/12_pi.cpp
+++ 02_openmp/12_pi.cpp
@@ -4,7 +4,7 @@ int main() {
   int n = 10;
   double dx = 1. / n;
   double pi = 0;
-#pragma omp paralell for
+#pragma omp paralell for reduction(+:pi)
   for (int i=0; i<n; i++) {
     double x = (i + 0.5) * dx;
     pi += 4.0 / (1.0 + x * x) * dx;
